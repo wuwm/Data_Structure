8c6a57ab834f8609c7a17ea5c50a05753f31e0f3
吴未名 <Wu@wuweimingdembp.lan>
2014-10-16 22:06:14 +0800

    Initial Commit

---
+++ Data_Structure/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Data_Structure
+//
+//  Created by 吴未名 on 14-10-16.
+//  Copyright (c) 2014年 吴未名. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
