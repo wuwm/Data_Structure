52005ae6a154af1d01d0e6061c7e42e0cb3cf13f
吴未名 <Wu@wuweimingdembp.lan>
2014-10-16 22:12:02 +0800

    add printf

--- Data_Structure/main.c
+++ Data_Structure/main.c
@@ -7,12 +7,6 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[])
-{
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void main(){
+    printf("hello world");
 }
-
