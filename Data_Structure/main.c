4e42fafac22b0c9f713a9b9661da3b13c3f2abbd
吴未名 <Wu@wuweimingdembp.lan>
2014-10-17 13:14:18 +0800

    INITLIST_SQ

--- Data_Structure/main.c
+++ Data_Structure/main.c
@@ -7,6 +7,20 @@
 //
 
 #include <stdio.h>
-void main(){
-    printf("hello world");
+#define LIST_INIT_SIZE 10
+typedef struct {
+    int *a;
+    int length;
+    int listsize;
+}SqList;
+int InitList_Sq(SqList *L){
+    L->a=(int *)malloc(sizeof(int)*LIST_INIT_SIZE);
+    if(!L->a) exit(1);
+    L->length=0;
+    L->listsize=LIST_INIT_SIZE;
+    return 1;
+    
+}
+int main(){
+    return 0;
 }
